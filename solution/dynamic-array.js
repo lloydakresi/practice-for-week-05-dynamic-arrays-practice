228d06655832173c2437282864bd4df366463964
Lloyd Akresi <lloydakresi03@gmail.com>
2022-12-31 17:48:50 +0000

    Completed- passes all tests

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -3,17 +3,25 @@ class DynamicArray {
   constructor(defaultSize=4) {
 
     // Your code here
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(defaultSize);
 
   }
 
   read(index) {
-
     // Your code here
+    return this.data[index];
   }
 
   unshift(val) {
-
     // Your code here
+    if (this.length === 0) this.data[0] = val;
+    for(let i = this.length; i >= 0; i--){
+      this.data[i] = this.data[i - 1]
+    }
+    this.data[0] = val;
+    this.length++;
   }
 
 }
